0ff2c59fb4c4b3f8ef2d190e4eb665a79af01249
Andrii-Mitko <andreymit123@gmail.com>
2026-06-10 20:49:14 +0300

    add \notes\[id]\page.tsx

---
+++ app/(private routes)/notes/[id]/page.tsx
@@ -0,0 +1,56 @@
+import { Metadata } from "next";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
+
+import { fetchNoteById } from "@/lib/api";
+import NoteDetailsClient from "./NoteDetails.client";
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const note = await fetchNoteById(params.id);
+
+  return {
+    title: note.title,
+    description: note.content.slice(0, 120),
+
+    openGraph: {
+      title: note.title,
+      description: note.content.slice(0, 120),
+      url: `http://localhost:3000/notes/${params.id}`,
+      images: [
+        {
+          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+          width: 1200,
+          height: 630,
+          alt: note.title,
+        },
+      ],
+      type: "article",
+    },
+  };
+}
+
+export default async function Page({ params }: Props) {
+  const { id } = params;
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient id={id} />
+    </HydrationBoundary>
+  );
+}
